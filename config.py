4c4f9c61281c1355928c88a58cddd8c44ce670d1
PseudoSkull <pseudoskull1@gmail.com>
2023-09-28 22:12:52 -0500

    OCR correction

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # WS_collection
 
 username = "PseudoSkull"
-mainspace_work_title = "Pratt Portraits"
+mainspace_work_title = "A Literary Courtship"
 transcription_prefix = f"User:{username}/P/"
 transcription_page_title = transcription_prefix + mainspace_work_title
